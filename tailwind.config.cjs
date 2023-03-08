465c874cedc97b5c6df50fd230815044a19ce73b
Joabesv <joabevarjao123@gmail.com>
2023-03-08 14:53:33 -0300

    configure custom styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 /* eslint-env node */
+const palette = require('./palette');
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: palette,
+    },
+    fontFamily: {
+      regular: ['RobotoRegular'],
+      medium: ['RobotoMedium'],
+      bold: ['RobotoBold'],
+      black: ['RobotoBlack'],
+    },
   },
   plugins: [],
-}
+};
